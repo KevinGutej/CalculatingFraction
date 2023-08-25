4ed81b1f2741ad62dceb7e79ad69f5ed1bb85105
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-08-25 21:31:44 +0200

    Calculating Greatest Common Divisor

--- CalculatingFractions/index.php
+++ CalculatingFractions/index.php
@@ -8,5 +8,21 @@ class Fraction {
         if ($denominator == 0) {
             throw new Exception("Denominator can't be zero'");
         }
+
+        $this->numerator = $numerator;
+        $this->denominator = $denominator;
+        $this->simplify();
+    }
+
+     private function simplify() {
+        $greatestCommonDivisor = $this->calculateGreatestCommonDivisor($this->numerator, $this->denominator);
+        $this->numerator /= $greatestCommonDivisor;
+        $this->denominator /= $greatestCommonDivisor;
+    }
+
+    private function calculateGreatestCommonDivisor($a, $b) {
+        if ($b == 0) {
+            return $a;
+        }
     }
 }
